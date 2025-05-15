5cd41d822e403678a6d0291f8b15293356c3b461
Fujii Makoto <147135481+makoty-pg@users.noreply.github.com>
2025-05-15 14:12:44 +0900

    get_period_scores()の変更と，make_message.pyの変更及び動作確認

--- backend/functions/make_message.py
+++ backend/functions/make_message.py
@@ -1,21 +1,28 @@
 
-def make_message(period_scores: list):
+def make_message(period_scores: list, cl_num: int):
     messages = ""
+    classroom = ['本日のＲ３ー３０１教室における環境をお知らせいたします\n\n', 
+                 '本日のＲ３ー４０１教室における環境をお知らせいたします\n\n', 
+                 '本日のＲ３ー４０３教室における環境をお知らせいたします\n\n']
+    cl_mes = classroom[cl_num]
     for i in range(len(period_scores)):
-        message = ""
-        message += "{}限目\n".format(i+1)
-        message += "*CO2のスコア: {}*\n".format(period_scores[i][0])
-        message += "*温度のスコア: {}*\n".format(min(period_scores[i][1], period_scores[i][2]))
-        message += "*湿度のスコア: {}*\n".format(min(period_scores[i][3], period_scores[i][4]))
-        alert_messages = ""
-        alert_messages += alert_co2(period_scores[i][0])
-        alert_messages += alert_temperature(period_scores[i][1], period_scores[i][2])
-        alert_messages += alert_humidity(period_scores[i][3], period_scores[i][4])
-        if(alert_messages == ""):
-            alert_messages = "良い環境でした！"
-        message += "評価: {}\n".format(alert_messages)
-        messages += message
-    return messages
+        if period_scores[i] != None:
+            message = ""
+            message += "{}限目\n".format(i+1)
+            message += "*CO2のスコア: {}*\n".format(period_scores[i][0])
+            message += "*温度のスコア: {}*\n".format(min(period_scores[i][1], period_scores[i][2]))
+            message += "*湿度のスコア: {}*\n".format(min(period_scores[i][3], period_scores[i][4]))
+            alert_messages = ""
+            alert_messages += alert_co2(period_scores[i][0])
+            alert_messages += alert_temperature(period_scores[i][1], period_scores[i][2])
+            alert_messages += alert_humidity(period_scores[i][3], period_scores[i][4])
+            if(alert_messages == ""):
+                alert_messages = "良い環境でした！"
+            message += "評価: {}\n".format(alert_messages)
+            messages += message
+    
+    send_message = cl_mes + messages
+    return send_message
 
 def alert_co2(co2_score: int):
     if co2_score == 1:
