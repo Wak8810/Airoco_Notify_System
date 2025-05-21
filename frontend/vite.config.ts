79023391b5cf247b41056b22949dd65d73c52ed1
Wak8810 <231205192@ccmailg.meijo-u.ac.jp>
2025-05-21 15:23:51 +0900

    gh-pages用の変更

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,10 +14,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: undefined,
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'assets/index-[hash].css';
-          return 'assets/[name]-[hash][extname]';
-        },
+        assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
       },
